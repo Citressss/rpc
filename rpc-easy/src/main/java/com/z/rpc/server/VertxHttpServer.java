c8245de68644e51ed94d706a363d6164d3b32400
Citres <2313576248@qq.com>
2024-05-05 21:02:38 +0800

    为VertxHttpServer绑定请求处理器HttpServerHandler

--- rpc-easy/src/main/java/com/z/rpc/server/VertxHttpServer.java
+++ rpc-easy/src/main/java/com/z/rpc/server/VertxHttpServer.java
@@ -2,8 +2,16 @@ package com.z.rpc.server;
 
 import io.vertx.core.*;
 
+/**
+ * Vertx HTTP 服务器
+ */
 public class VertxHttpServer implements HttpServer {
 
+    /**
+     * 启动服务器
+     *
+     * @param port
+     */
     public void doStart(int port) {
         // 创建 Vert.x 实例
         Vertx vertx = Vertx.vertx();
@@ -12,15 +20,7 @@ public class VertxHttpServer implements HttpServer {
         io.vertx.core.http.HttpServer server = vertx.createHttpServer();
 
         // 监听端口并处理请求
-        server.requestHandler(request -> {
-            // 处理 HTTP 请求
-            System.out.println("Received request: " + request.method() + " " + request.uri());
-
-            // 发送 HTTP 响应
-            request.response()
-                    .putHeader("content-type", "text/plain")
-                    .end("Hello from Vert.x HTTP server!");
-        });
+        server.requestHandler(new HttpServerHandler());
 
         // 启动 HTTP 服务器并监听指定端口
         server.listen(port, result -> {
