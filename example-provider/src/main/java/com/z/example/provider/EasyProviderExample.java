b654eabe07ff02c5a351645c21f5a1d32ac846b2
Citres <2313576248@qq.com>
2024-05-05 20:04:49 +0800

    注册服务，jdk序列化器

--- example-provider/src/main/java/com/z/example/provider/EasyProviderExample.java
+++ example-provider/src/main/java/com/z/example/provider/EasyProviderExample.java
@@ -1,5 +1,7 @@
 package com.z.example.provider;
 
+import com.z.example.common.service.UserService;
+import com.z.rpc.registry.LocalRegistry;
 import com.z.rpc.server.HttpServer;
 import com.z.rpc.server.VertxHttpServer;
 
@@ -15,6 +17,9 @@ import com.z.rpc.server.VertxHttpServer;
  */
 public class EasyProviderExample {
     public static void main(String[] args) {
+        // 注册服务
+        LocalRegistry.register(UserService.class.getName(),UserServiceImpl.class);
+
         // 启动 web 服务
         HttpServer httpServer = new VertxHttpServer();
         httpServer.doStart(8080);
