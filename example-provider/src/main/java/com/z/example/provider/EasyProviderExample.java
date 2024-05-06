b4c5df90065fde4155349ec7fa24a2afa2c5521b
Citres <2313576248@qq.com>
2024-05-06 16:26:45 +0800

    修改监听端口为配置文件指定端口

--- example-provider/src/main/java/com/z/example/provider/EasyProviderExample.java
+++ example-provider/src/main/java/com/z/example/provider/EasyProviderExample.java
@@ -1,6 +1,7 @@
 package com.z.example.provider;
 
 import com.z.example.common.service.UserService;
+import com.z.rpc.RpcApplication;
 import com.z.rpc.registry.LocalRegistry;
 import com.z.rpc.server.HttpServer;
 import com.z.rpc.server.VertxHttpServer;
@@ -17,11 +18,12 @@ import com.z.rpc.server.VertxHttpServer;
  */
 public class EasyProviderExample {
     public static void main(String[] args) {
+        RpcApplication.init();
         // 注册服务
         LocalRegistry.register(UserService.class.getName(),UserServiceImpl.class);
 
         // 启动 web 服务
         HttpServer httpServer = new VertxHttpServer();
-        httpServer.doStart(8080);
+        httpServer.doStart(RpcApplication.getRpcConfig().getServerPort());
     }
 }
