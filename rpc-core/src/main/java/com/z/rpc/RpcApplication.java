96bc9038d3b5658494d7b06b293fb047398a3030
Citres <2313576248@qq.com>
2024-05-15 15:50:53 +0800

    初始化注册中心

--- rpc-core/src/main/java/com/z/rpc/RpcApplication.java
+++ rpc-core/src/main/java/com/z/rpc/RpcApplication.java
@@ -1,7 +1,10 @@
 package com.z.rpc;
 
+import com.z.rpc.config.RegistryConfig;
 import com.z.rpc.config.RpcConfig;
 import com.z.rpc.constant.RpcConstant;
+import com.z.rpc.registry.Registry;
+import com.z.rpc.registry.RegistryFactory;
 import com.z.rpc.utils.ConfigUtils;
 import lombok.extern.slf4j.Slf4j;
 
@@ -22,6 +25,11 @@ public class RpcApplication {
     public static void init(RpcConfig newRpcConfig) {
         rpcConfig = newRpcConfig;
         log.info("rpc init, config = {}", newRpcConfig.toString());
+        // 注册中心初始化
+        RegistryConfig registryConfig = rpcConfig.getRegistryConfig();
+        Registry registry = RegistryFactory.getInstance(registryConfig.getRegistry());
+        registry.init(registryConfig);
+        log.info("registry init, config = {}", registryConfig);
     }
 
     /**
