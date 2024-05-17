13b54e02fbfc2bab2e5f906554c6f66d96b7052d
Citres <2313576248@qq.com>
2024-05-17 21:50:35 +0800

    处理请求死锁bug修复

--- rpc-core/src/main/java/com/z/rpc/server/tcp/VertxTcpServer.java
+++ rpc-core/src/main/java/com/z/rpc/server/tcp/VertxTcpServer.java
@@ -1,11 +1,8 @@
 package com.z.rpc.server.tcp;
 
 import com.z.rpc.server.HttpServer;
-import io.vertx.core.Handler;
 import io.vertx.core.Vertx;
-import io.vertx.core.buffer.Buffer;
 import io.vertx.core.net.NetServer;
-import io.vertx.core.parsetools.RecordParser;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -20,37 +17,7 @@ public class VertxTcpServer implements HttpServer {
         NetServer server = vertx.createNetServer();
 
         // 处理请求
-        server.connectHandler(socket -> {
-            // 构造 parser
-            RecordParser parser = RecordParser.newFixed(8);
-            parser.setOutput(new Handler<Buffer>() {
-                // 初始化
-                int size = -1;
-                // 一次完整的读取（头 + 体）
-                Buffer resultBuffer = Buffer.buffer();
-
-                @Override
-                public void handle(Buffer buffer) {
-                    if (-1 == size) {
-                        // 读取消息体长度
-                        size = buffer.getInt(4);
-                        parser.fixedSizeMode(size);
-                        // 写入头信息到结果
-                        resultBuffer.appendBuffer(buffer);
-                    } else {
-                        // 写入体信息到结果
-                        resultBuffer.appendBuffer(buffer);
-                        System.out.println(resultBuffer.toString());
-                        // 重置一轮
-                        parser.fixedSizeMode(8);
-                        size = -1;
-                        resultBuffer = Buffer.buffer();
-                    }
-                }
-            });
-
-            socket.handler(parser);
-        });
+        server.connectHandler(new TcpServerHandler());
 
         // 启动 TCP 服务器并监听指定端口
         server.listen(port, result -> {
