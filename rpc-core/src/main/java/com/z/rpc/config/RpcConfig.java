ab9e53ca5c3225d79d1115f54d2ac15b3cf95351
Citres <2313576248@qq.com>
2024-05-18 17:02:40 +0800

    实现负载均衡器与配置扩展

--- rpc-core/src/main/java/com/z/rpc/config/RpcConfig.java
+++ rpc-core/src/main/java/com/z/rpc/config/RpcConfig.java
@@ -1,5 +1,6 @@
 package com.z.rpc.config;
 
+import com.z.rpc.loadbalancer.LoadBalancerKeys;
 import com.z.rpc.serializer.SerializerKeys;
 import lombok.Data;
 
@@ -43,4 +44,9 @@ public class RpcConfig {
      * 注册中心配置
      */
     private RegistryConfig registryConfig = new RegistryConfig();
+
+    /**
+     * 负载均衡器
+     */
+    private String loadBalancer = LoadBalancerKeys.ROUND_ROBIN;
 }
---
+++ rpc-core/src/main/java/com/z/rpc/loadbalancer/LoadBalancerKeys.java
@@ -0,0 +1,17 @@
+package com.z.rpc.loadbalancer;
+
+/**
+ * 负载均衡器键名常量
+ */
+public interface LoadBalancerKeys {
+
+    /**
+     * 轮询
+     */
+    String ROUND_ROBIN = "roundRobin";
+
+    String RANDOM = "random";
+
+    String CONSISTENT_HASH = "consistentHash";
+
+}
