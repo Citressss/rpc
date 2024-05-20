d64216d01930f395970338cdfd3e5f89d90a2bcc
Citres <2313576248@qq.com>
2024-05-20 16:22:32 +0800

    init

---
+++ rpc-spring-boot-starter/src/test/java/com/z/rpcspringbootstarter/RpcSpringBootStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.z.rpcspringbootstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpcSpringBootStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
