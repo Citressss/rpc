55772a4f165b9a931d9cb1b15160b889fcc8a8ff
Citres <2313576248@qq.com>
2024-05-20 20:29:24 +0800

    注解启动测试

--- example-springboot-consumer/src/main/java/com/z/springboot/consumer/ExampleSpringbootConsumerApplication.java
+++ example-springboot-consumer/src/main/java/com/z/springboot/consumer/ExampleSpringbootConsumerApplication.java
@@ -7,9 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 @EnableRpc(needServer = false)
 public class ExampleSpringbootConsumerApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ExampleSpringbootConsumerApplication.class, args);
     }
-
 }
