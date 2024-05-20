86f36e37e7ef457c10b94782805c2ef50edf4634
Citres <2313576248@qq.com>
2024-05-20 19:46:10 +0800

    spring版服务提供者与服务消费者init

---
+++ example-springboot-provider/src/main/java/com/z/springboot/provider/ExampleSpringbootProviderApplication.java
@@ -0,0 +1,15 @@
+package com.z.springboot.provider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class ExampleSpringbootProviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleSpringbootProviderApplication.class, args);
+	}
+
+}
